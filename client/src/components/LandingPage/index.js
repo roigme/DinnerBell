fac1410493560a5cdb3732c6059f4d067340bd2f
Matthew Timko <timko.matthew@gmail.com>
2018-10-04 18:44:31 -0400

    search bar homepage work

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
-import './LandingPage.css';
+import React, { Component } from "react";
+import "./LandingPage.css";
 
 class LandingPage extends Component {
-
-
-render() {
+  render() {
     return (
-        <div className="lp-wrapper">
+      <div className="lp-wrapper">
         <div className="lp-text">
-        <span className="lp"><i class="fas fa-search-location"></i> Search</span><br />
-        <span className="lp"><i className="far fa-comments"></i> Connect</span><br />
-        <span className="lp"><i class="fas fa-utensils"></i> Eat</span>
+          <span className="lp">
+            <i class="fas fa-search-location" /> Search
+          </span>
+          <br />
+          <span className="lp">
+            <i className="far fa-comments" /> Connect
+          </span>
+          <br />
+          <span className="lp">
+            <i class="fas fa-utensils" /> Eat
+          </span>
         </div>
-        </div>
-    )
-}
+        <div class="home-search">
+          <select className="custom-select custom-select-lg mb-3">
+          <i className="fas fa-users"></i>
 
+            <option selected>Party Size?</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="3">4</option>
+            <option value="3">5</option>
+            <option value="3">6</option>
+            <option value="3">7</option>
+            <option value="3">8</option>
+            <option value="3">9</option>
+            <option value="3">10</option>
+            <option value="3">11</option>
+            <option value="3">12</option>
+          </select>
+
+          <select className="custom-select custom-select-lg mb-3">
+            <option selected>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+        </div>
+        <button type="button" class="btn btn-primary btn-lg btn-block home-search-btn">
+         Search
+        </button>
+      </div>
+    );
+  }
 }
 
 export default LandingPage;
