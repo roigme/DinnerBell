b215ca8365aace52c30e9842d2bc36bfc4de1fa8
pat <patlehmann1@gmail.com>
2018-10-05 15:05:57 -0400

    added Matty's SignUpPage, set up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from './components/LandingPage';
 import ButtonAppBar from './components/ButtonAppBar';
 import AboutPage from './components/AboutPage';
+import LogInPage from './components/LogInPage';
+import SignUpPage from './components/SignUpPage';
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-      <div className="main-element">
-      <div className="overlay">
+      <div className='wrapper'>
+      <div className='main-element'>
+      <div className='overlay'>
+      <Router>
+      <div>
       <ButtonAppBar />
-      <LandingPage />
+      <Route exact path='/' component={LandingPage} />
+      <Route exact path='/login' component={LogInPage} />
+      <Route path='/signup' component={SignUpPage} />
+      </div>
+      </Router>
       </div>
       </div>
       <AboutPage />
--- client/src/components/ButtonAppBar/index.js
+++ client/src/components/ButtonAppBar/index.js
@@ -1,39 +1,63 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './ButtonAppBar.css'
 
 function ButtonAppBar() {
   return (
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark text-white main-nav">
-  <div className="icon-brand-wrapper">
-  <img src={require('./images/iconfinder-256.png')} alt="dinner plate" className='plate-icon' />
-  <a className="navbar-brand" href="#">Dinner Bell</a>
-  </div>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-white main-nav">
+      <div className="icon-brand-wrapper">
+        <img src={require('./images/iconfinder-256.png')} alt="dinner plate" className='plate-icon' />
+      </div>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-  <div className="collapse navbar-collapse text-white" id="navbarSupportedContent">
-   <ul className="navbar-nav mr-auto">
-    <li className="nav-item">
-     <a className="nav-link" href="#">About</a>
-    </li>
-    <li className="nav-item">
-     <a className="nav-link" href="#">Login</a>
-    </li>
-    <li className="nav-item">
-     <a className="nav-link" href="#">Sign Up</a>
-    </li>
-    <li className="nav-item dropdown">
-     <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-      <a className="dropdown-item" href="#">About</a>
-      <a className="dropdown-item" href="#">Login</a>
-      <div className="dropdown-divider"></div>
-      <a className="dropdown-item" href="#">Sign Up</a>
-     </div>
-    </li>
-   </ul>
-  </div>
- </nav>
+      <div className="collapse navbar-collapse text-white" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" ? "nav-link active text-white" : "nav-link text-white"
+              }
+            >
+              Dinner Bell
+      </Link>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#aboutpage-top">About</a>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/login"
+              className={
+                window.location.pathname === "/login" ? "nav-link active" : "nav-link"
+              }
+            >
+              Log In
+      </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/signup"
+              className={
+                window.location.pathname === "/signup" ? "nav-link active" : "nav-link"
+              }
+            >
+              Sign Up
+      </Link>
+          </li>
+          <li className="nav-item dropdown">
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item">About</a>
+              <a className="dropdown-item">Login</a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item">Sign Up</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
