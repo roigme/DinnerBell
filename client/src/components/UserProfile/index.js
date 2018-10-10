5e3f5741dbf2a0e6166941936f1065c5d631ac7e
Matthew Timko <timko.matthew@gmail.com>
2018-10-09 21:37:49 -0400

    added profile

--- client/src/components/UserProfile/index.js
+++ client/src/components/UserProfile/index.js
@@ -1,157 +1,188 @@
 import React, { Component } from "react";
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import SideNav, {
+  Toggle,
+  Nav,
+  NavItem,
+  NavIcon,
+  NavText
+} from "@trendmicro/react-sidenav";
 import "./UserProfile.css";
 
 class UserProfile extends Component {
   render() {
     return (
-        <div className="container emp-profile">
+      <div className="container emp-profile">
         <form method="post">
-            <div className="row">
-                <div className="col-md-4">
-                    <div className="profile-img">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt=""/>
-                        <div className="file btn btn-lg btn-primary">
-                            Change Photo
-                            <input type="file" name="file"/>
-                        </div>
-                    </div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="profile-img">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog"
+                  alt=""
+                />
+                <div className="file btn btn-lg btn-primary">
+                  Change Photo
+                  <input type="file" name="file" />
                 </div>
-                <div className="col-md-6">
-                    <div className="profile-head">
-                                <h5>
-                                    Kshiti Ghelani
-                                </h5>
-                                <h6>
-                                    Web Developer and Designer
-                                </h6>
-                                <p className="proile-rating">RANKINGS : <span>8/10</span></p>
-                        <ul className="nav nav-tabs" id="myTab" role="tablist">
-                            <li className="nav-item">
-                                <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Timeline</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="col-md-2">
-                    <input type="submit" className="profile-edit-btn" name="btnAddMore" value="Edit Profile"/>
-                </div>
+              </div>
             </div>
-            <div className="row">
-                <div className="col-md-4">
-                    <div className="profile-work">
-                        <p>WORK LINK</p>
-                        <a href="">Website Link</a><br/>
-                        <a href="">Bootsnipp Profile</a><br/>
-                        <a href="">Bootply Profile</a>
-                        <p>SKILLS</p>
-                        <a href="">Web Designer</a><br/>
-                        <a href="">Web Developer</a><br/>
-                        <a href="">WordPress</a><br/>
-                        <a href="">WooCommerce</a><br/>
-                        <a href="">PHP, .Net</a><br/>
-                    </div>
-                </div>
-                <div className="col-md-8">
-                    <div className="tab-content profile-tab" id="myTabContent">
-                        <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <label>User Id</label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <p>Kshiti123</p>
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <label>Name</label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <p>Kshiti Ghelani</p>
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <label>Email</label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <p>kshitighelani@gmail.com</p>
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <label>Phone</label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <p>123 456 7890</p>
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <label>Profession</label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <p>Web Developer and Designer</p>
-                                        </div>
-                                    </div>
-                        </div>
-                        <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <label>Experience</label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <p>Expert</p>
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <label>Hourly Rate</label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <p>10$/hr</p>
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <label>Total Projects</label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <p>230</p>
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <label>English Level</label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <p>Expert</p>
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <label>Availability</label>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <p>6 months</p>
-                                        </div>
-                                    </div>
-                            <div className="row">
-                                <div className="col-md-12">
-                                    <label>Your Bio</label><br/>
-                                    <p>Your detail description</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div className="col-md-6">
+              <div className="profile-head">
+                <h5>Kshiti Ghelani</h5>
+                <h6>Dinner Bell Member</h6>
+                <p className="proile-rating">
+                  RANKINGS : <span>8/10</span>
+                </p>
+                <ul className="nav nav-tabs" id="myTab" role="tablist">
+                  <li className="nav-item">
+                    <a
+                      className="nav-link active"
+                      id="home-tab"
+                      data-toggle="tab"
+                      href="#home"
+                      role="tab"
+                      aria-controls="home"
+                      aria-selected="true"
+                    >
+                      Diner Profile
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      id="profile-tab"
+                      data-toggle="tab"
+                      href="#profile"
+                      role="tab"
+                      aria-controls="profile"
+                      aria-selected="false"
+                    >
+                      Host Profile
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
-        </form>           
-    </div>
+            <div className="col-md-2">
+              <input
+                type="submit"
+                className="profile-edit-btn"
+                name="btnAddMore"
+                value="Edit Profile"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="profile-work">
+                <ul>
+                  <h3>Favorite Foods</h3>
+                  <li>French Fries</li>
+                  <li>Pizza</li>
+                  <li>Burritos</li>
+                  <li>Tacos</li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-">
+              <div className="tab-content profile-tab" id="myTabContent">
+                <div
+                  className="tab-pane fade show active"
+                  id="home"
+                  role="tabpanel"
+                  aria-labelledby="home-tab"
+                >
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>Username</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Kshiti123</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>Name</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Kshiti Ghelani</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>Location</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Richmond, Virginia</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>Party Size (Including You!)</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>5</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-12">
+                      <label>Bio</label>
+                      <br />
+                      <p>Tell us about yourself!</p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="tab-pane fade"
+                  id="profile"
+                  role="tabpanel"
+                  aria-labelledby="profile-tab"
+                >
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>Currently Hosting?</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Yes</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>Max Party Size</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>10</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>Days Available</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Fri-Sun</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>Location</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Richmond, VA</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-12">
+                      <label>Menu</label>
+                      <br />
+                      <p>What do you like to cook?</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     );
   }
 }
