580baa1d183c73e4f5a549f7f1e504dcf484d210
pat <patlehmann1@gmail.com>
2018-10-09 21:12:11 -0400

    deleted axios delete methods

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -9,10 +9,6 @@ export default {
   getUserByID: function(id) {
     return axios.get("/api/user/" + id);
   },
-  // Deletes the user with the given id
-  deleteUser: function(id) {
-    return axios.delete("/api/user/" + id);
-  },
   // Saves a user to the database
   saveUser: function(userData) {
     return axios.post("/api/user", userData);
@@ -24,10 +20,6 @@ export default {
   // Gets the host with the given id
   getHostByID: function(id) {
     return axios.get("/api/host/" + id);
-  },
-  // Deletes the host with the given id
-  deleteHost: function(id) {
-    return axios.delete("/api/host/" + id);
   },
   // Saves a host to the database
   saveHost: function(hostData) {
